1d269385286f756efac1fc549d0c173a0be86af1
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-03-24 02:38:49 -0300

    add EntradaDeDados

---
+++ EntradaDeDados.java
@@ -0,0 +1,17 @@
+public class EntradaDeDados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Lê nome completo, quantidade de quartos na casa e preço do produto separados
+        String fullName = sc.nextLine();
+        int qtBedrooms = sc.nextInt();
+        double productPrice = sc.nextDouble();
+
+        // Lê último nome, idade e altura da mesma linha
+        String lastName = sc.next();
+        int age = sc.nextInt();
+        double height = sc.nextDouble();
+
+        sc.close();
+    }
+}

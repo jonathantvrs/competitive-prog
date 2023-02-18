f8819af0e94b3a42c8702d9f4d84a9579072af0f
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-02-18 19:19:41 -0300

    salary 1008.py resolved

---
+++ Uri1008.py
@@ -0,0 +1,8 @@
+number = int(input())
+worked_hours = int(input())
+dolar_per_hour = float(input())
+
+salary = worked_hours * dolar_per_hour
+
+print(f"NUMBER = {number}")
+print("SALARY = U$ {:.2f}".format(salary))

95087e17a577677d2130b57ddd0310302f196719
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-02-21 16:01:50 -0300

    banknotes 1018 resolved

--- beginners/Uri1018.py
+++ beginners/Uri1018.py
@@ -1,25 +1,16 @@
 n = int(input())
 
-print(n)
-
-amount_of_100 = n // 100
+print(n) 
+print(str(n // 100) + " nota(s) de R$ 100,00")
 n %= 100
-amount_of_50 = n // 50 
+print(str(n // 50) + " nota(s) de R$ 50,00")
 n %= 50
-amount_of_20 = n // 20 
+print(str(n // 20) + " nota(s) de R$ 20,00")
 n %= 20
-amount_of_10 = n // 10
+print(str(n // 10) + " nota(s) de R$ 10,00")
 n %= 10
-amount_of_5 = n // 5
+print(str(n // 5) + " nota(s) de R$ 5,00")
 n %= 5
-amount_of_2 = n // 2
+print(str(n // 2) + " nota(s) de R$ 2,00")
 n %= 2
-amount_of_1 = n // 1   
-
-print(f"{amount_of_100} nota(s) de R$ 100,00")
-print(f"{amount_of_50} nota(s) de R$ 50,00")
-print(f"{amount_of_20} nota(s) de R$ 20,00")
-print(f"{amount_of_10} nota(s) de R$ 10,00")
-print(f"{amount_of_5} nota(s) de R$ 5,00")
-print(f"{amount_of_2} nota(s) de R$ 2,00")
-print(f"{amount_of_1} nota(s) de R$ 1,00")
+print(str(n // 1) + " nota(s) de R$ 1,00")

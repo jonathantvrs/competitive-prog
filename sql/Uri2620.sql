47350b6a3284fa613bf8830bb6279014ff7f01ce
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-02-22 17:41:27 -0300

    orders in first half sql 2620 resolved

--- sql/Uri2620.sql
+++ sql/Uri2620.sql
@@ -2,4 +2,11 @@ SELECT c.name, o.id
 FROM customers c, orders o
 WHERE o.id_customers = c.id AND
     o.orders_date >= '2016-01-01' AND 
-    o.orders_date <= '2016-06-30'
+    o.orders_date <= '2016-06-30';
+
+-- OR
+
+SELECT c.name, o.id
+FROM customers c, orders o
+WHERE o.id_customers = c.id AND
+    o.orders_date BETWEEN '2016-01-01' AND '2016-06-30';
